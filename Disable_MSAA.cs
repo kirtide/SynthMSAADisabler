29b250731a26bb007dbe6b91342f740c3c5d8602
Kirtide <Kirtide@KIRTIDE-DESKTOP>
2025-04-05 02:42:28 +1100

    Add project files.

---
+++ Disable_MSAA.cs
@@ -0,0 +1,46 @@
+﻿using MelonLoader;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace SynthMSAA
+{
+    public class MSAAMod : MelonMod
+    {
+        private bool noMSAA = false;
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (!noMSAA)
+            {
+                ForceMSAA();
+                //have it load in its own function, it looks nicer on the console lol
+            }
+        }
+
+        private void ForceMSAA()
+        {
+            var urpAssets = Resources.FindObjectsOfTypeAll<UniversalRenderPipelineAsset>(); //indexes all or most loaded pipelines in memory idk
+            foreach (var urpAsset in urpAssets) //make available
+            {
+                if (urpAsset.name == "Ultra_PipelineAsset") //must be the pc pipeline, this check could be skipped
+                {
+                    MelonLogger.Msg($"Found pipeline: {urpAsset.name}"); //prints actual name of pipeline
+                    MelonLogger.Msg($"Boot MSAA is: {urpAsset.msaaSampleCount}"); //prints bootup msaa setting
+                    if (urpAsset.msaaSampleCount != (int)MsaaQuality.Disabled) //simple int check
+                    {
+                        urpAsset.msaaSampleCount = (int)MsaaQuality.Disabled; //sets the integer value to 1
+                        MelonLogger.Msg($"MSAA set to Disabled ({urpAsset.msaaSampleCount})");
+                        noMSAA = true; //properly disabled
+                    }
+                    else
+                    {
+                        MelonLogger.Msg("MSAA already Disabled");
+                    }
+                    return;
+                }
+            }
+            MelonLogger.Msg("Ultra_PipelineAsset not found among URP assets");
+            noMSAA = true; //couldnt find pc pipeline, possibly prevents forking
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using SynthMSAA;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MSAA Disabler")]
+[assembly: AssemblyDescription("Disables MSAA at runtime")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSAA Disabler")]
+[assembly: AssemblyCopyright("© 2025 Kirtide")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//required melonloader stuff
+[assembly: MelonInfo(typeof(MSAAMod), "MSAA Disabler", "1.0.0", "Kirtide")]
+[assembly: MelonGame("Kluge Interactive", "SynthRiders")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b36a3bbf-711f-472d-ad43-8044a9cd8f02")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
