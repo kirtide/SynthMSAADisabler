a30427d5343481a8d8ab5064712ddeffa6467f9e
Kirtide <kirtide@hotmail.com.au>
2025-04-06 14:22:25 +1000

    removed check for specific pipeline, this is probably fine

--- Disable_MSAA.cs
+++ Disable_MSAA.cs
@@ -22,25 +22,24 @@ namespace SynthMSAA
             var urpAssets = Resources.FindObjectsOfTypeAll<UniversalRenderPipelineAsset>(); //indexes all or most loaded pipelines in memory idk
             foreach (var urpAsset in urpAssets) //make available
             {
-                if (urpAsset.name == "Ultra_PipelineAsset") //must be the pc pipeline, this check could be skipped
+                MelonLogger.Msg($"Found pipeline: {urpAsset.name}"); //prints actual name of pipeline
+                //at this point, we are assuming this is the current and only active pipeline
+                //this part will likely change if found inaccurate, if so we'll need to index all available pipelines in memory, then run something like a switch case
+                MelonLogger.Msg($"Boot MSAA is: {urpAsset.msaaSampleCount}");
+                if (urpAsset.msaaSampleCount != (int)MsaaQuality.Disabled) //simple int check, if no equals do the following
                 {
-                    MelonLogger.Msg($"Found pipeline: {urpAsset.name}"); //prints actual name of pipeline
-                    MelonLogger.Msg($"Boot MSAA is: {urpAsset.msaaSampleCount}"); //prints bootup msaa setting
-                    if (urpAsset.msaaSampleCount != (int)MsaaQuality.Disabled) //simple int check
-                    {
-                        urpAsset.msaaSampleCount = (int)MsaaQuality.Disabled; //sets the integer value to 1
-                        MelonLogger.Msg($"MSAA set to: {urpAsset.msaaSampleCount} (Disabled)");
-                        noMSAA = true; //properly disabled
-                    }
-                    else
-                    {
-                        MelonLogger.Msg("MSAA already Disabled");
-                    }
-                    return;
+                    urpAsset.msaaSampleCount = (int)MsaaQuality.Disabled; //sets the integer value to 1
+                    MelonLogger.Msg($"MSAA set to: {urpAsset.msaaSampleCount} (Disabled)");
+                    noMSAA = true; //we're done
                 }
+                else
+                {
+                    MelonLogger.Msg("MSAA already Disabled");
+                }
+                return;
             }
             MelonLogger.Msg("Ultra_PipelineAsset not found among URP assets");
-            noMSAA = true; //couldnt find pc pipeline, possibly prevents forking
+            noMSAA = true; //couldnt find pc pipeline, this is a possibly unneeded preventative step
         }
     }
 }
